965f55f151857827c43dafd12f3a56b2481db33a
bakul14 <m_bakulevskiy@baumanracing.ru>
2024-02-18 20:12:58 +0300

    (+) added net=host docker run argument

--- create_aderc.sh
+++ create_aderc.sh
@@ -4,6 +4,7 @@ rm .aderc
 
 echo -e export ADE_DOCKER_RUN_ARGS=\"$'\n'\
 $'\t'--cap-add=SYS_PTRACE$'\n'\
+$'\t'--net=host'\n'\
 $'\t'-v /home/$USER/.ssh:/$USER/.ssh:ro\"$'\n'\
 export ADE_IMAGES=\"$'\n'\
 $'\t'brtcyphal/cyphal_tools:cyphal$'\n'\
