8047841d7c420744f91d1d9a7e3ef5341bf0a73c
bakul14 <m_bakulevskiy@baumanracing.ru>
2024-02-19 12:11:25 +0300

    (-)line associated with videocard has been remved from the create_aderc script

--- create_aderc.sh
+++ create_aderc.sh
@@ -3,7 +3,6 @@ cd "$(dirname "$0")"
 rm .aderc
 
 echo -e export ADE_DOCKER_RUN_ARGS=\"$'\n'\
-$'\t'--cap-add=SYS_PTRACE$'\n'\
 $'\t'--net=host'\n'\
 $'\t'-v /home/$USER/.ssh:/$USER/.ssh:ro\"$'\n'\
 export ADE_IMAGES=\"$'\n'\
